1cc175e711e3172391c6f294735d4d321844e2d6
AllSungho <nanumeran@gmail.com>
2025-09-03 17:35:37 +0900

    feature / task : 엔티티 생성

---
+++ src/main/java/org/example/taskflow/domain/task/entity/Task.java
@@ -0,0 +1,27 @@
+package org.example.taskflow.domain.task.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.example.taskflow.common.entity.SoftDeletableEntity;
+import org.example.taskflow.domain.task.enums.Description;
+import org.example.taskflow.domain.task.enums.Priority;
+import org.example.taskflow.domain.task.enums.Status;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class Task extends SoftDeletableEntity {
+
+    private String title;
+    private String contents;
+    private Priority priority;
+    private Status status;
+    private Description description;
+    private LocalDateTime deadline;
+}
---
+++ src/main/java/org/example/taskflow/domain/task/enums/Description.java
@@ -0,0 +1,12 @@
+package org.example.taskflow.domain.task.enums;
+
+public enum Description {
+
+    TASK_CREATED,
+    TASK_UPDATED,
+    TASK_DELETED,
+    TASK_STATUS_CHANGED,
+    COMMENT_CREATED,
+    COMMENT_UPDATED,
+    COMMENT_DELETED,
+}
