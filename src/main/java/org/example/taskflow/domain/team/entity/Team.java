490b9d48008ab9285c6575f1c5189c9e3b07638b
Johs <206869862+desembers@users.noreply.github.com>
2025-09-04 15:04:05 +0900

    feat/team : 아이디 삭제 설명100자이내 ì, 이름은 10자이내

--- src/main/java/org/example/taskflow/domain/team/entity/Team.java
+++ src/main/java/org/example/taskflow/domain/team/entity/Team.java
@@ -2,6 +2,7 @@ package org.example.taskflow.domain.team.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.taskflow.common.entity.BaseEntity;
@@ -14,11 +15,20 @@ import org.example.taskflow.domain.user.entity.User;
 @Table(name = "team")
 public class Team extends BaseEntity {
 
+    @Column(length = 10)
     private String name;
 
-    @Column(nullable = false, unique = true)
+    @Id
+    @Column(nullable = false, length = 50)
     private Long teamId;
 
+    @Column(length = 100)
     private String explain;
+
+    @Builder
+    public Team(String name, String explain) {
+        this.name = name;
+        this.explain = explain;
+    }
 
 }
