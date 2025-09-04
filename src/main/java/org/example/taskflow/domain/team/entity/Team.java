3924196f394f06363abc9a88726cc794f587d911
Johs <206869862+desembers@users.noreply.github.com>
2025-09-04 15:16:51 +0900

    ãid를 PathVariableí(ãteamId)

--- src/main/java/org/example/taskflow/domain/team/entity/Team.java
+++ src/main/java/org/example/taskflow/domain/team/entity/Team.java
@@ -11,16 +11,11 @@ import org.example.taskflow.domain.user.entity.User;
 @Entity
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "team")
 public class Team extends BaseEntity {
 
     @Column(length = 10)
     private String name;
-
-    @Id
-    @Column(nullable = false, length = 50)
-    private Long teamId;
 
     @Column(length = 100)
     private String explain;
