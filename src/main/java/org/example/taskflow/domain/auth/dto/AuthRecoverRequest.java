da0c31e2ac865874defa78ecb496549a130c47c4
BibliyaSeo <seongkyeong.seo@gmail.com>
2025-09-05 13:54:25 +0900

    feature/auth-user: 회원탈퇴, 회원복구

---
+++ src/main/java/org/example/taskflow/domain/auth/dto/AuthRecoverRequest.java
@@ -0,0 +1,16 @@
+package org.example.taskflow.domain.auth.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AuthRecoverRequest {
+    @NotBlank(message = "사용자 이름은 필수입니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+}
