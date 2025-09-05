e1f83fc63becdab5c75c84ee02348849954914af
LeeDongJae-KR <movementj98@gmail.com>
2025-09-05 15:05:24 +0900

    feature/task: 태스크 조회 관련 서비스 구현 완료

---
+++ src/main/java/org/example/taskflow/domain/dashboard/service/TasksService.java
@@ -0,0 +1,44 @@
+package org.example.taskflow.domain.dashboard.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.taskflow.domain.dashboard.repository.TasksRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class TasksService implements TaskServiceImpl {
+
+    private final TasksRepository tasksRepository;
+
+
+    @Override
+    public int countByAssigneeId(Long assigneeId) {
+        return tasksRepository.countByAssigneeId(assigneeId);
+    }
+
+    @Override
+    public int countByAssigneeIdAndStatus(Long assigneeId, String status) {
+        return tasksRepository.countByAssigneeIdAndStatus(assigneeId, status);
+    }
+
+    @Override
+    public int countOverdueTasksByAssigneeId(Long assigneeId, LocalDate date) {
+
+        //현재 시간 기준으로 기한이 지난 작업 조회
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        return tasksRepository.countOverdueTasksByAssigneeId(assigneeId,currentDateTime);
+    }
+
+    @Override
+    public int countTodayTasksByAssigneeId(Long assigneeId, LocalDate date) {
+        LocalDateTime startOfDay = date.atStartOfDay();
+        LocalDateTime endOfDay = date.atTime(LocalTime.MAX);
+        return tasksRepository.countTodayTasksByAssigneeId(assigneeId,startOfDay,endOfDay);
+    }
+}
