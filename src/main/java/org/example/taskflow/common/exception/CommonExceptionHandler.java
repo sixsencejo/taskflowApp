fba0acd13bebaf6d41201cde7b4a726ffeaa7dd7
seongmin2223 <khyuno9@naver.com>
2025-09-08 00:45:43 +0900

    feat/activity : aop, Activity관련 기능추가

--- src/main/java/org/example/taskflow/common/exception/CommonExceptionHandler.java
+++ src/main/java/org/example/taskflow/common/exception/CommonExceptionHandler.java
@@ -1,5 +1,6 @@
 package org.example.taskflow.common.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.taskflow.common.dto.CommonResponse;
 import org.example.taskflow.common.utils.ResponseUtil;
 import org.springframework.http.ResponseEntity;
@@ -7,13 +8,15 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class CommonExceptionHandler {
 
     // 커스텀 예외 처리
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<CommonResponse<Object>> handleCustomException(CustomException e){
+    public ResponseEntity<CommonResponse<Object>> handleCustomException(CustomException e) {
         CommonResponse<Object> commonResponse = ResponseUtil.fail(e.getMessage());
+        log.error("handleCustomException={}", commonResponse);
         return ResponseEntity
                 .status(e.getErrorCode().getStatus())
                 .body(commonResponse);
@@ -21,15 +24,19 @@ public class CommonExceptionHandler {
 
     // Valid 예외 처리
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<CommonResponse<Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+    public ResponseEntity<CommonResponse<Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String message = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
         CustomException customException = new CustomException(ErrorCode.INVALID_REQUEST_PARAMETER, message);
+        log.warn("handleMethodArgumentNotValidException={}", customException);
         return handleCustomException(customException);
     }
 
     // 일반 예외 처리
     @ExceptionHandler(Exception.class)
     public ResponseEntity<CommonResponse<Object>> handleException(Exception e) {
+
+        log.error("Unhandled server exception occurred", e);
+
         CustomException customException = new CustomException(ErrorCode.PROCESSING_ERROR);
         return handleCustomException(customException);
     }
