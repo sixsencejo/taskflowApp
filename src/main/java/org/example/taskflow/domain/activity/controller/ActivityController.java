60fc9dbc4bf1a3414ab6a7886b5b57d1f7c1178a
seongmin2223 <khyuno9@naver.com>
2025-09-08 21:54:46 +0900

    refactor/activity : Cors관련 리팩토링(CrossOrigin전체제거)

--- src/main/java/org/example/taskflow/domain/activity/controller/ActivityController.java
+++ src/main/java/org/example/taskflow/domain/activity/controller/ActivityController.java
@@ -15,14 +15,16 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
 
 @RestController
 @RequestMapping("/activities")
-@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 public class ActivityController {
 
