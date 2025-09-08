331157d82b98fb94c4efdf60de79b4157692753a
LeeDongJae-KR <movementj98@gmail.com>
2025-09-09 06:26:22 +0900

    feature/user: 담당자 엔티티에 정적 팩토리 메서드 추가 완료

--- src/main/java/org/example/taskflow/domain/task/dto/Assignee.java
+++ src/main/java/org/example/taskflow/domain/task/dto/Assignee.java
@@ -1,5 +1,7 @@
 package org.example.taskflow.domain.task.dto;
 
+import org.example.taskflow.domain.user.entity.User;
+
 public record Assignee(
 
         Long id,
@@ -7,4 +9,12 @@ public record Assignee(
         String name,
         String email
 ) {
+    public static Assignee from(User user) {
+        return new Assignee(
+                user.getId(),
+                user.getUsername(),
+                user.getName(),
+                user.getEmail()
+        );
+    }
 }
